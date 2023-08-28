ccf0caa50e798050cf0e3935dceb8ef933f88bc2
Nonpawit Pothinil <mosaggpothinil@gmail.com(Nonpawit Pothinil)>
2023-08-28 21:40:07 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const uuid = require("uuid")
+const speakeasy = require("speakeasy")
+const PORT = process.env.PORT || 5000
+const { JsonDB } = require("node-json-db")
+const { Config } = require("node-json-db/dist/lib/JsonDBConfig")
+
+const app = express();
+app.use(express.json())
+
+const db = new JsonDB(new Config('myDatabase', true, false, '/'));
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Welcome to api"
+    })
+})
+
+app.post('/api/register', (req, res) => {
+    const id = uuid.v4();
+
+    try {
+        const path = `/user/${id}`
+        const temp_secret = speakeasy.generateSecret()
+        db.push(path, { id, temp_secret })
+        res.json({ id, secret: temp_secret.base32 })
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: "Error generating secret" })
+    }
+})
+
+app.post('/api/verify', async (req, res) => {
+    const { token, userId } = req.body;
+
+    
+    try {
+        const path = `/user/${userId}`
+        const user = await db.getData(path)
+        const { base32:secret } = user.temp_secret
+
+        const verified = speakeasy.totp.verify({ secret, encoding: 'base32', token: token });
+    
+        if (verified) {
+            db.push(path, { id: userId, secret: user.temp_secret});
+            res.json({ verified: true })
+        } else {
+            res.json({ verified: false })
+        }
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: "Error verifying" })
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
